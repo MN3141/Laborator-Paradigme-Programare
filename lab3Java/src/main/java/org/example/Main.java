908703c36d7499b9fb6fd4d5cf8be02c6c6ceedc
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-03-12 09:33:57 +0200

    Lab3 structuri date

---
+++ lab3Java/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList tab=new ArrayList();
+        for(int i=0;i<=14;i++) {
+            tab.add((int)(Math.random() * 10 + 1));
+            System.out.println(tab.get(i));}
+    }
+}
